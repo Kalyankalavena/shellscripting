83b8d882b9c36d023172fecf06039b33b444f378
Kalyankalavena <kalyankalaven@gmail.com>
2025-09-03 07:35:18 +0530

    added variables script

---
+++ 02-variables.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+echo "sahithi: : Hi sindhu"
+echo "sindhu: : Hi sahithi"
+echo "sahithi: : How are you?"
+echo "sindhu: : I am good, what about you?"
+echo "sahithi: : I am good too."
