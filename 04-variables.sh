c6ca6068fd777b96a2f741757379f3705dad6e52
Kalyankalavena <kalyankalaven@gmail.com>
2025-09-03 09:59:52 +0530

    converstion added

---
+++ 04-variables.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+person1="$1"
+person2="$2"
+
+echo "$person1: : Hi $person2"
+echo "$person2: : Hi $person1"
+echo "$person1: : How are you?"
+echo "$person2: : I am good, what about you?"
+echo "$perrson1: : I am good too"
+echo "$person2: : Great to hear that!" 
