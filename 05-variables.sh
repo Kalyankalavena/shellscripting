dfe8cadd2520c2399e094c8c5875a93ed11b62b7
Kalyan DevSecOps <kalyankalaven@gmail.com>
2025-12-24 14:11:07 +0530

    Delete 05-variables.sh

--- 05-variables.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "please enter your username::" # text entered here  will be added as variable
-
-read:s USERNAME # here username is a variable name
-
-echo " username : $USERNAME"
-
