ec9f4ada8e1832ea7628c32060330d337f59c528
Kalyan DevSecOps <kalyankalaven@gmail.com>
2025-12-24 14:10:27 +0530

    Delete 02-variables.sh

--- 02-variables.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-echo "sahithi: : Hi sindhu"
-echo "sindhu: : Hi sahithi"
-echo "sahithi: : How are you?"
-echo "sindhu: : I am good, what about you?"
-echo "sahithi: : I am good too."
